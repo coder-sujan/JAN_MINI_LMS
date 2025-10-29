d5049a163a4277cf796637aef0bf9df6704bd0f3
coder-sujan <Sujan.aryal@coderacademy.edu.au>
2025-10-29 20:55:44 +1100

    Feature added validation message for empty tables

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -1,6 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from init import db
-from models.student import Student
+from models.student import Student, student_schema, students_schema
 
 
 students_bp = Blueprint("students", __name__, url_prefix="/students")
@@ -10,13 +10,22 @@ students_bp = Blueprint("students", __name__, url_prefix="/students")
 # Our Routesto be defined....
 
 # GET /students
+
+@students_bp.route("/")
+def get_students():
+    #first define a statement: Select * from students;
+    stmt = db.select(Student)
+    #execute it
+    students_list = db.session.scalars(stmt)
+    #serialise it
+    data = students_schema.dump(students_list)
+    #return the jsonify(list)
+    if data:
+        return jsonify(data)
+    else:
+        return {"message": "No Records Found!. Add a student to get started with system..."}
+    
 # GET /students/id
 # POST /students
 # PUT/PATCH /students/id
 # DELETE /students/id
-
-
-@app.route("/")
-  def a_fun():
-      return "this fun"
-
