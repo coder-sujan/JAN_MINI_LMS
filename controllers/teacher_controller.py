34bea581484368dd7989d5a936e6164d1b124b38
coder-sujan <Sujan.aryal@coderacademy.edu.au>
2025-11-05 20:38:47 +1100

    Feature: TEacher Post and Delete method added

--- controllers/teacher_controller.py
+++ controllers/teacher_controller.py
@@ -55,3 +55,73 @@ def get_a_teacher(teacher_id):
     else:
         return {"message": f"Teacher with id: {teacher_id} does not exist..."}
         
+        
+        
+
+# POST /students
+@teachers_bp.route("/", methods=["POST"])
+def create_teachers():
+    # get details from request body
+    body_data = request.get_json()
+    #create a student object with the request body data
+    # email = body_data.get("email")
+    
+    stmt = db.select(Student).where(Student.email == email)
+    #adding to the session
+    teacher = db.session.scalar(stmt)
+    #checking the system with sam email add (validfation)
+    data = teacher_schema.dump(teacher)
+    
+    #display message with same email add isuues...
+    # if data:
+    #     return {"message": f"The Teacher with email:{email} already exists. suggestions ()"}
+     
+    new_teacher = Teacher(
+        name = body_data.get("name"),
+        department = body_data.get("department"),
+        address = body_data.get("address")
+    )
+    #add to the session
+    db.session.add(new_teacher)
+    # Commit the session
+    db.session.commit()
+    #send Ack
+    #creating a new variable data and stoing everything in there
+    data = teacher_schema.dump(new_teacher)
+    #calling that data to convert with jsonify 
+    return jsonify(data), 201
+#fileds cannot be empty
+
+#Email unique error
+
+#default errors - unexpected error occured.
+
+#404 error occurd!
+
+
+
+# DELETE /students/id
+@teachers_bp.route("/<int:teacher_id>", methods=["DELETE"])
+def delete_teacher(teacher_id):
+    #find the std with id. : Select * from student where student_id = student_id
+    stmt = db.select(Teacher).where(Teacher.teacher_id == teacher_id)
+    teacher = db.session.scalar(stmt)
+    #if std exists:
+    if teacher:
+        
+       #detele
+       name = teacher.name
+       db.session.delete(teacher)
+       #commit
+       db.session.commit()
+       #return ack
+       return {"message": f"Teacher with name {teacher.name} deleted successfully"}, 200
+    else:
+       #retun ack
+       return {"message": f"Teacher with id: {teacher} does not exixt..."}, 404
+    
+    #else
+       # return ack
+       
+       
+    
