1f429f36ca6c68a4f7c43b5b41be16c1bf0c1d15
coder-sujan <Sujan.aryal@coderacademy.edu.au>
2025-11-10 20:58:58 +1100

    Feature: Added get all courses

--- controllers/course_controller.py
+++ controllers/course_controller.py
@@ -5,3 +5,33 @@ from models.course import Course, course_schema, courses_schema
 
 courses_bp = Blueprint("courses", __name__, url_prefix="/courses")
 
+# CREATE - POST /courses
+
+# READ - GET /courses and get /course/id
+
+@courses_bp.route("/")
+def get_courses():
+    #dif the statement
+    stmt = db.select(Course)
+    #ex it
+    courses_list = db.session.scalars(stmt)
+    
+    
+    #serialse it
+    data = courses_schema.dump(courses_list)
+    #if it exists:
+    if data:
+        
+        #return it
+        return jsonify(data)
+        
+    #else:
+    else :
+        #something else
+        return{"message": "No Courses Created or Assigned Yet!!"}, 404
+
+
+# UPDATE - PUT/PATCH /courses/id
+
+
+# DELETE - DELETE /courses/id
